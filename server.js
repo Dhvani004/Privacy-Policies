355435d95e4207975f3df6c47a218eda621b8eed
Dhvani004 <dhvanikhandelwal894@gmail.com>
2026-04-24 16:44:57 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+
+// TicTacToe
+app.get("/tictactoe", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/tictactoe/index.html"));
+});
+
+app.get("/tictactoe/privacy-policy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/tictactoe/privacy.html"));
+});
+
+// TextToSpeech
+app.get("/texttospeech", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/texttospeech/index.html"));
+});
+
+app.get("/texttospeech/privacy-policy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/texttospeech/privacy.html"));
+});
+
+// Default
+app.get("/", (req, res) => {
+  res.send("Server Running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
